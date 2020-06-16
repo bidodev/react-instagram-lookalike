619bb50aa29c950be7ece0f2d246636a488f86fa
Claudinei Bido <bido.c@live.com>
2020-06-16 13:20:52 +0200

    added font-awesome as dependencie

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React, { Component } from "react";
-import './sass/main.scss'
-import Header from "./components/header/header-component";
-import Profile from "./components/profile/profile-component";
-import Content from "./components/inner/inner-component";
+import React, { Component } from 'react';
+import './sass/main.scss';
+import Header from './components/header/header-component';
+import Profile from './components/profile/profile-component';
+import Content from './components/inner/inner-component';
 
 import * as content from './data.json';
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       content
-    }
+    };
   }
   render() {
     return (
+      // wrapping div-container
       <div className="App">
         <Header />
         <div className="main">
-        
-        <Profile />
-        <Content data={this.state.content.default}/>
+          <Profile />
+          <Content data={this.state.content.default} />
+        </div>
       </div>
-      </div>
-
     );
   }
 }
--- src/components/inner/navigation/navigation-component.jsx
+++ src/components/inner/navigation/navigation-component.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './navigation-component-styles.scss'
 
 const Navigation = () => {
-    return ( <div className="navigation">Hello navigation</div> );
-}
- 
+    return (
+        <ul className="navigation">
+            <li>Posts</li>
+            <li>Igtv</li>
+            <li>Tagged</li>
+        </ul> );
+        }
+         
 export default Navigation;
