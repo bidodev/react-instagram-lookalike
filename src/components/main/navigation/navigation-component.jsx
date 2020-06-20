1f2ae275fa6dafbf9599099405eaa39670cb9e82
Claudinei Bido <bido.c@live.com>
2020-06-20 21:26:18 +0200

    starting to implemment routing on the project

--- src/components/main/navigation/navigation-component.jsx
+++ src/components/main/navigation/navigation-component.jsx
@@ -1,17 +1,60 @@
 import React from 'react';
 import './navigation-component-styles.scss';
-import NavigationItem from './navigation-item/navigation-item.component'
+//import NavigationItem from './navigation-item/navigation-item.component'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Posts = () => {
+  return ( <div>Posts</div> );
+}
+const Igtv = () => {
+  return ( <div>Igtv</div> );
+}
+const Tagged = () => {
+  return ( <div>Tagged</div> );
+}
+ 
+
 
 const Navigation = () => {
   return (
+    <Router>
     <div className="navigation">
-      <ul>
-        <NavigationItem className={"active"} iconName={"stream"} title={"posts"} />
-        <NavigationItem iconName={"video"} title={"Igtv"} />
-        <NavigationItem iconName={"id-card-alt"} title={"Tagged"} />
-      </ul>
-    </div>
+        <ul>
+          <li>
+            <Link to="/">Posts</Link>
+          </li>
+          <li>
+            <Link to="/igtv">Igtv</Link>
+          </li>
+          <li>
+            <Link to="/tagged">Tagged</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/">
+            <Posts />
+          </Route>
+          <Route path="/igtv">
+            <Igtv />
+          </Route>
+          <Route path="/tagged">
+            <Tagged />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
 export default Navigation;
+
+{/* <NavigationItem className={"active"} iconName={"stream"} title={"Posts"} />
+<NavigationItem iconName={"video"} title={"Igtv"} />
+<NavigationItem iconName={"id-card-alt"} title={"Tagged"} /> */}
