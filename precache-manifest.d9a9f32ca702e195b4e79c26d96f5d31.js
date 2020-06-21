5f75b89d68ff04f4c028ef75d97cd294d5f4f24a
Claudinei Bido <bido.c@live.com>
2020-06-21 11:36:40 +0200

    Updates

---
+++ precache-manifest.d9a9f32ca702e195b4e79c26d96f5d31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efb369ee4a863c89b349ba4fb492cd7",
+    "url": "/instagram-lookalike/index.html"
+  },
+  {
+    "revision": "b3d205d718e9c10d29e6",
+    "url": "/instagram-lookalike/static/css/main.a6b826d4.chunk.css"
+  },
+  {
+    "revision": "1f141199e6dec578e8ba",
+    "url": "/instagram-lookalike/static/js/2.97b8127b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/instagram-lookalike/static/js/2.97b8127b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3d205d718e9c10d29e6",
+    "url": "/instagram-lookalike/static/js/main.0f73d778.chunk.js"
+  },
+  {
+    "revision": "c538229a7f9fabc55159",
+    "url": "/instagram-lookalike/static/js/runtime-main.b8f4f7f2.js"
+  }
+]);
